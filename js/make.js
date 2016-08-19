32c8a81e052303b5b267a70cccd8594a8527a6a8
Jonathan Ward <j@w4rd.com>
2016-08-18 19:10:35 -0500

    cleanup

---
+++ js/make.js
@@ -0,0 +1,245 @@
+
+
+
+
+function make(){
+
+var material = {feed:250,plunge:200}
+var pass_depth = 0.425
+var filetype = "sbp"
+
+g=""
+
+pins.reverse()
+
+for(i=0;i<net.length;i++){
+	for(j=0;j<net[i].length;j++){
+		if(net[i][j].D==true){
+			for(k=0;k<net.length;k++){
+				for(l=0;l<net[k].length;l++){
+					if( (net[i][j].X.toFixed(2)==net[k][l].X.toFixed(2)) && (net[i][j].Y.toFixed(2)==net[k][l].Y.toFixed(2)) && (net[k][l].D==true) ){
+						if((i!=k)||(l!=j)){
+							net[k][l].D=false
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+	if(document.getElementById('side').value=="top"){
+		flip()
+	}
+
+if(filetype=="gcode"){
+
+
+}
+
+else if(filetype=="sbp"){
+
+pass_depth = 0.0175
+
+g+="MS," + (material.feed/25.4/60).toFixed(1) + "," + (material.plunge/25.4/60).toFixed(1) + "\n"
+g+="JZ,0.2\n"
+g+="SO,1,1\n"
+g+="PAUSE 1\n"   
+
+
+if((document.getElementById("board").value)=="arduino"){
+
+   for(i=0;i<vias.length;i++){
+
+		pass_no = 4
+		pass = 1
+		g+="J2,"+((vias[i][0].X+Math.abs(xmin))/25.4).toFixed(4)+","+ ((vias[i][0].Y+ymax)/25.4).toFixed(4) + "\n"
+
+		while(pass<=pass_no){
+	   g+="MZ,-"+ (pass_depth*pass).toFixed(4) + "\n"
+			for(j=0;j<vias[i].length;j++){
+   	   	g+="M2,"+((vias[i][j].X+Math.abs(xmin))/25.4).toFixed(4) +","+ ((vias[i][j].Y+ymax)/25.4).toFixed(4) + "\n"
+			}
+		pass++
+   	}
+		g+="JZ,0.1\n"
+	}
+
+   for(i=0;i<pins.length;i++){
+		g+="J2,"+((pins[i][0].X+Math.abs(xmin))/25.4).toFixed(4)+","+ ((pins[i][0].Y+ymax)/25.4).toFixed(4) + "\n"
+	   g+="MZ,-"+ (0.007) + "\n"
+		g+="PAUSE 0.1\n"
+			for(j=1;j<pins[i].length;j++){
+				g+="M2,"+((pins[i][j].X+Math.abs(xmin))/25.4).toFixed(4)+","+ ((pins[i][j].Y+ymax)/25.4).toFixed(4) + "\n"		
+			}
+		g+="JZ,0.1\n"
+	}
+
+}
+
+	for(i=0;i<hole.length;i++){
+
+		pass_no = 4
+		pass = 1
+		g+="J2,"+((hole[i][0].X+Math.abs(xmin))/25.4).toFixed(4)+","+ ((hole[i][0].Y+ymax)/25.4).toFixed(4) + "\n"
+
+		while(pass<=pass_no){
+	   g+="MZ,-"+ (pass_depth*pass).toFixed(4) + "\n"
+			for(j=0;j<hole[i].length;j++){
+   	   	g+="M2,"+((hole[i][j].X+Math.abs(xmin))/25.4).toFixed(4) +","+ ((hole[i][j].Y+ymax)/25.4).toFixed(4) + "\n"
+			}
+		pass++
+   	}
+		g+="JZ,0.1\n"
+	}
+
+   for(i=0;i<net.length;i++){
+		
+   	for(j=0;j<net[i].length;j++){
+		pass_no = 5
+		pass = 1
+		if(net[i][j].D==true){
+			g+="J2,"+((net[i][j].X+Math.abs(xmin))/25.4).toFixed(4)+","+ ((net[i][j].Y+ymax)/25.4).toFixed(4) + "\n"
+				while(pass<=pass_no){
+	   			g+="MZ,-"+ (pass_depth*pass).toFixed(4) + "\n"
+					g+="JZ,0.05\n"
+					pass++
+				}
+			g+="JZ,0.1\n"
+		}
+		}
+	}
+
+	outlines.reverse()
+
+   for(i=0;i<outlines.length;i++){
+		g+="J2,"+((outlines[i][0].X/scale/25.4)+Math.abs(xmin)/25.4).toFixed(4)+","+ (((outlines[i][0].Y/scale)+ymax)/25.4).toFixed(4) + "\n"
+	   g+="MZ,-"+ (0.007) + "\n"
+		g+="PAUSE 0.1\n"
+			for(j=1;j<outlines[i].length;j++){
+				g+="M2,"+((outlines[i][j].X/scale/25.4)+Math.abs(xmin)/25.4).toFixed(4)+","+ (((outlines[i][j].Y/scale)+ymax)/25.4).toFixed(4) + "\n"		
+			}
+		g+="JZ,0.1\n"
+	}
+
+pass_no = 4
+pass = 1
+
+
+g+="J2,"+((path1[0].X+Math.abs(xmin))/25.4).toFixed(4)+","+ ((path1[0].Y+ymax)/25.4).toFixed(4) + "\n"
+
+while(pass<=pass_no){
+   
+   g+="MZ,-"+ (pass_depth*pass).toFixed(4) + "\n"
+   for(i=1;i<path1.length;i++){
+      g+="M2,"+((path1[i].X+Math.abs(xmin))/25.4).toFixed(4) +","+ ((path1[i].Y+ymax)/25.4).toFixed(4) + "\n"
+   }
+   pass++
+}
+
+
+g+="JZ,0.2\n"
+g+="SO,1,0\n"
+if((document.getElementById("board").value)=="arduino"){
+	g+="J3," + "0" + "," + ((ymax+Math.abs(ymin)+0.2)/25.4).toFixed(4) + ",0.4\n"
+}
+if((document.getElementById("board").value)=="blank"){
+	g+="J3," + ((xmax+Math.abs(xmin)+0.2)/25.4).toFixed(4) + "," + "0" + ",0.4\n"
+}
+
+if((document.getElementById("board").value)=="arduino"){
+	var jobFile = 'shield.sbp'
+	var jobName = 'Arduino Shield'
+}
+else if((document.getElementById("board").value)=="blank"){
+	jobFile = 'pcb.sbp'
+	jobName = 'PCB'
+}
+
+fabmo.submitJob({
+   file : g,
+   filename : jobFile,
+   name : jobName,
+	description : (((xmax+Math.abs(xmin))/25.4).toFixed(2)) + " x " + (((ymax+Math.abs(ymin))/25.4).toFixed(2)) + "\" " + "(1/32\" endmill)"  
+});
+
+
+}
+
+	g=""
+
+	if(document.getElementById('side').value=="top"){
+		flip()
+	}
+
+}
+
+function flip(){
+
+   for(i=0;i<outlines.length;i++){
+		for(j=0;j<outlines[i].length;j++){
+			if((outlines[i][j].Y)<0){
+				outlines[i][j]={X:outlines[i][j].X,Y:(Math.abs(outlines[i][j].Y))}
+			}
+			else{
+				outlines[i][j]={X:outlines[i][j].X,Y:(0-(outlines[i][j].Y))}	
+			}
+		}
+	}
+
+   for(i=0;i<pins.length;i++){
+		for(j=0;j<pins[i].length;j++){
+			if(pins[i][j].Y<0){
+				pins[i][j].Y=Math.abs(pins[i][j].Y)
+			}
+			else{
+				pins[i][j].Y=(0-pins[i][j].Y)	
+			}
+		}
+	}
+
+   for(i=0;i<vias.length;i++){
+		for(j=0;j<vias[i].length;j++){
+			if(vias[i][j].Y<0){
+				vias[i][j].Y=Math.abs(vias[i][j].Y)
+			}
+			else{
+				vias[i][j].Y=(0-vias[i][j].Y)	
+			}
+		}
+	}
+
+   for(i=0;i<hole.length;i++){
+		for(j=0;j<hole[i].length;j++){
+			if(hole[i][j].Y<0){
+				hole[i][j].Y=Math.abs(hole[i][j].Y)
+			}
+			else{
+				hole[i][j].Y=(0-hole[i][j].Y)	
+			}
+		}
+	}
+
+
+   for(i=0;i<net.length;i++){
+		for(j=0;j<net[i].length;j++){
+			if(net[i][j].Y<0){
+				net[i][j].Y=Math.abs(net[i][j].Y)
+			}
+			else{
+				net[i][j].Y=(0-net[i][j].Y)	
+			}
+		}
+	}
+
+   for(i=0;i<path1.length;i++){	
+		if(path1[i].Y<0){
+			path1[i]={X:path1[i].X,Y:(Math.abs(path1[i].Y))}
+		}
+		else{
+			path1[i]={X:path1[i].X,Y:(0-(path1[i].Y))}	
+		}
+	}
+
+}
+
---
+++ js/parts.js
@@ -0,0 +1,448 @@
+
+
+
+
+
+function parts(){
+
+	arduino.push( [{X:-34+o,Y:-26.5},{X:31+o,Y:-26.5},{X:31+o,Y:-13.5},{X:34+o,Y:-10.5},{X:34+o,Y:21.5},{X:31+o,Y:24.5},{X:31+o,Y:26.5},{X:-34+o,Y:26.5},{X:-34+o,Y:22.5}] )
+
+	arduino.push([])
+	arduino.push([])
+	arduino.push([])
+	arduino.push([])
+
+	arduino.push( [{X:-34+o,Y:13.5},{X:-34+o,Y:-5.5}] )
+	arduino.push( [{X:-34+o,Y:-17.5},{X:-34+o,Y:-26.5}] )
+	arduino.push( [{X:-40+o,Y:-17.5},{X:-23+o,Y:-17.5},{X:-23+o,Y:-5.5},{X:-40+o,Y:-5.5},{X:-40+o,Y:-17.5}] )
+	arduino.push( [{X:-35+o,Y:13.5},{X:-22+o,Y:13.5},{X:-22+o,Y:22.5},{X:-35+o,Y:22.5},{X:-35+o,Y:13.5}] )
+
+	makeGrid()	
+
+	x = -17.8
+	//top pins
+	pin_outlines.push([{X:(x+o-0.635+0.42),Y:-20.32-0.42},{X:(x+o-0.635+0.42),Y:-21.86},{X:(x+o)+0.42,Y:-22.86},{X:(x+o)+0.42,Y:-25.4+0.4},{X:(x+o+2.54)+(2.54*9)-0.4,Y:-25.4+0.4},{X:(x+o+2.54)+(2.54*9)-0.4,Y:-23.6},{X:(x+o+3.175)+(2.54*9)-0.42-1.27,Y:-22.86},{X:(x+o+3.175)+(2.54*9)-0.42-1.27,Y:-20.32-0.42},{X:(x+o-0.635+0.42),Y:-20.32-0.42}])
+
+	x = x + 2.54	
+
+	for(i=0;i<9;i++){
+		pins.push([{X:(x+o-0.635),Y:-20.32},{X:(x+o-0.635),Y:-21.86},{X:(x+o),Y:-22.86},{X:(x+o),Y:-25.4}])
+	x = x + 2.54
+	}
+
+	x = 9.505
+	
+	pin_outlines.push([{X:(x+o+0.42),Y:-20.32-0.2},{X:(x+o+0.42),Y:-25.4+0.4},{X:(x+o+2.54*8-0.42),Y:-25.4+0.4},{X:(x+o+2.54*8-0.42),Y:-20.32-0.4},{X:(x+o+0.42),Y:-20.32-0.4}])
+
+	x = x + 2.54
+	for(i=0;i<7;i++){
+		pins.push([{X:(x+o),Y:-20.32},{X:(x+o),Y:-25.4}])
+	x = x + 2.54
+	}
+	//bottom pins
+
+	x = 29.825
+
+	pin_outlines.push([{X:(x+o)-0.42,Y:20.32+0.4},{X:(x+o)-0.42,Y:25.4-0.4},{X:(x+o-2.54*6)+0.42,Y:25.4-0.4},{X:(x+o-2.54*6)+0.42,Y:20.32+0.4},{X:(x+o)+0.42,Y:20.32+0.4}])
+
+	x = x - 2.54
+	for(i=0;i<5;i++){
+		pins.push([{X:(x+o),Y:20.32},{X:(x+o),Y:25.4}])
+	x = x - 2.54
+	}
+
+	x = 12.06
+
+	pin_outlines.push([{X:(x+o)-0.42,Y:20.32+0.4},{X:(x+o)-0.42,Y:25.4-0.4},{X:(x+o-2.54*8)+0.42,Y:25.4-0.4},{X:(x+o-2.54*8)+0.42,Y:20.32+0.4},{X:(x+o)-0.42,Y:20.32+0.4}])
+
+	x = x - 2.54
+	for(i=0;i<7;i++){
+		pins.push([{X:(x+o),Y:20.32},{X:(x+o),Y:25.4}])	
+	x = x - 2.54
+	}
+
+
+x=-17.10
+for(i=0;i<10;i++){
+	connect.push({X:(x+o),Y:(-21.61)})
+	x = x +2.54
+}
+
+x=10.84
+for(i=10;i<18;i++){
+	connect.push({X:(x+o),Y:(-21.61)})
+	x = x +2.54
+}
+
+x=28.575
+for(i=18;i<24;i++){
+	connect.push({X:(x+o),Y:(21.61)})
+	x = x -2.54
+}
+
+x=10.84
+for(i=24;i<31;i++){
+	connect.push({X:(x+o),Y:(21.61)})
+	x = x -2.54
+}
+
+	var x = 28.575
+	for(i=0;i<8;i++){
+		arduino_pins.push([])
+		vias.push([])
+			nodes.push({X:(x+o),Y:(-24)})
+			for(j=0;j<=25;j++){
+				arduino_pins[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.45),Y:((-24.15)+Math.cos((Math.PI*2)/25*j)*0.45)})
+				vias[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.1),Y:((-24.15)+Math.cos((Math.PI*2)/25*j)*0.1)})
+			}
+		x = x -2.54
+	}
+
+	x = 6.34
+	for(i=8;i<18;i++){
+		arduino_pins.push([])
+		vias.push([])
+		nodes.push({X:(x+o),Y:(-24)})
+			for(j=0;j<=25;j++){
+				arduino_pins[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.45),Y:((-24.15)+Math.cos((Math.PI*2)/25*j)*0.45)})
+				vias[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.1),Y:((-24.15)+Math.cos((Math.PI*2)/25*j)*0.1)})
+			}
+		x = x -2.54
+	}
+
+	var x = 28.575
+	for(i=18;i<24;i++){
+		arduino_pins.push([])
+		vias.push([])
+			nodes.push({X:(x+o),Y:(24)})
+			for(j=0;j<=25;j++){
+				arduino_pins[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.45),Y:((24.15)+Math.cos((Math.PI*2)/25*j)*0.45)})
+				vias[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.1),Y:((24.15)+Math.cos((Math.PI*2)/25*j)*0.1)})
+			}
+		x = x -2.54
+	}
+
+	var x = 10.84
+	for(i=24;i<32;i++){
+		arduino_pins.push([])
+		vias.push([])
+			nodes.push({X:(x+o),Y:(24)})
+			for(j=0;j<=25;j++){
+				arduino_pins[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.45),Y:((24.15)+Math.cos((Math.PI*2)/25*j)*0.45)})
+				vias[i].push({X:((x+o)+Math.sin((Math.PI*2)/25*j)*0.1),Y:((24.15)+Math.cos((Math.PI*2)/25*j)*0.1)})
+			}
+		x = x -2.54
+	}
+
+draw()
+route()
+
+}
+
+////
+
+function add_trace(){
+
+var traces = []
+
+//console.log(trace)
+
+if((document.getElementById("board").value)=="arduino"){
+
+for(i=0;i<pin_outlines.length;i++){
+	traces.push([])
+	for(j=0;j<pin_outlines[i].length;j++){
+		traces[traces.length-1].push({X:parseInt(pin_outlines[i][j].X*scale),Y:parseInt(pin_outlines[i][j].Y*scale)})
+	}
+}
+
+}//
+
+for(i=0;i<trace.length;i++){
+	traces.push([])
+	for(j=0;j<trace[i].length;j++){
+		traces[traces.length-1].push({X:parseInt(trace[i][j].X*scale),Y:parseInt(trace[i][j].Y*scale)})
+	}
+}
+
+//console.log(traces)
+
+/*
+var simple_paths = ClipperLib.Clipper.SimplifyPolygons(traces, ClipperLib.PolyFillType.pftNonZero)
+
+var cleandelta = 0.2; // 0.1 should be the appropriate delta in different cases
+traces = ClipperLib.JS.Clean(simple_paths, cleandelta * scale)
+*/
+
+var cpr = new ClipperLib.Clipper()
+cpr.AddPaths(traces, ClipperLib.PolyType.ptSubject, true)
+
+var solution_paths = new ClipperLib.Paths();
+cpr.Execute(ClipperLib.ClipType.ctUnion, solution_paths, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+
+for(i=0;i<solution_paths.length;i++){
+		solution_paths[i].reverse()
+		solution_paths[i].push(solution_paths[i][0])
+}
+
+solution_paths.push([])
+for(i=0;i<frame1.length;i++){
+	solution_paths[solution_paths.length-1].push({X:parseInt(frame1[i].X*scale),Y:parseInt(frame1[i].Y*scale)})
+}
+
+var co = new ClipperLib.ClipperOffset(scale*0.4, 0.25)
+//co.ArcTolerance = 1.23
+//co.MiterLimit = 0.7
+co.AddPaths(solution_paths, ClipperLib.JoinType.jtMiter, ClipperLib.EndType.etClosedPolygon, 0.25)
+
+var oset = []
+
+i=1
+var td = 0.39
+
+while(i<3){
+	offset = new ClipperLib.Paths()
+	co.Execute(offset, (-scale*td)*i)
+	i++
+	for(j=0;j<offset.length;j++){
+		oset.push(offset[j])
+	}
+}
+
+	offset=oset
+
+for(i=0;i<offset.length;i++){
+		offset[i].push(offset[i][0])
+}
+	outlines = offset
+
+draw()
+
+}
+
+////
+
+function add(){
+	if((temp2!=undefined)&&(pad==1)){
+
+	var deltaX = mouseX - origin.X
+	var deltaY = mouseY - origin.Y
+	var angle = (Math.atan2(deltaY, deltaX))
+
+	//console.log(angle)
+		
+	angle = ((Math.round(angle/(Math.PI/2)))*(Math.PI/2))
+
+		for(j=0;j<pkgOutlines[pkgOutlines.length-1].length;j++){
+			pkgOutlines[pkgOutlines.length-1][j]={X:(((resistorOutline[j].X)*(Math.cos(angle)))-((resistorOutline[j].Y)*(Math.sin(angle)))),Y:(((resistorOutline[j].Y)*(Math.cos(angle)))+((resistorOutline[j].X)*(Math.sin(angle))))}
+
+			pkgOutlines[pkgOutlines.length-1][j]={X:(pkgOutlines[pkgOutlines.length-1][j].X+origin.X),Y:(pkgOutlines[pkgOutlines.length-1][j].Y+origin.Y)}
+		}
+
+		for(j=0;j<temp.length;j++){
+			for(k=0;k<temp[j].length;k++){
+
+				temp[j][k]={X:(((temp2[j][k].X)*(Math.cos(angle)))-((temp2[j][k].Y)*(Math.sin(angle)))),Y:(((temp2[j][k].Y)*(Math.cos(angle)))+((temp2[j][k].X)*(Math.sin(angle))))}
+
+				temp[j][k]={X:(temp[j][k].X+origin.X).toFixed(2),Y:(temp[j][k].Y+origin.Y).toFixed(2)}
+
+			}
+		}
+
+	}
+}
+
+////
+
+function route(){
+
+for(i=0;i<frame1.length;i++){
+	outlines.push({X:parseInt(frame1[i].X*scale),Y:parseInt(frame1[i].Y*scale)})
+}
+	outlines = [outlines]
+
+for(i=0;i<pin_outlines.length;i++){
+	outlines.push([])
+	pin_outlines[i].reverse()
+	for(j=0;j<pin_outlines[i].length;j++){
+		outlines[outlines.length-1].push({X:parseInt(pin_outlines[i][j].X*scale),Y:parseInt(pin_outlines[i][j].Y*scale)})
+	}
+}
+
+var co = new ClipperLib.ClipperOffset(-scale*0.4, 0.25)
+co.AddPaths(outlines, ClipperLib.JoinType.jtMiter, ClipperLib.EndType.etClosedPolygon)
+offset = new ClipperLib.Paths()
+co.Execute(offset, -scale*0.4)
+
+for(i=0;i<offset.length;i++){
+		offset[i].push(offset[i][0])
+}
+
+add_trace()
+
+}
+
+////
+
+function makeFrame(radius) {
+
+if(document.getElementById("board").value=="arduino"){
+	var fo = 0
+}
+else{
+	fo = 0.2
+}
+frame1=[]
+path1=[]
+
+for (i=0;i<=verts;i++) {
+   if(i<=25){
+      frame1.push({X:((xmax+fo-radius)+Math.sin((Math.PI*2)/verts*i)*radius),Y:((ymax+fo-radius)+Math.cos((Math.PI*2)/verts*i)*radius)})
+      
+      path1.push({X:((xmax-radius)+Math.sin((Math.PI*2)/verts*i)*(radius+tool)),Y:((ymax-radius)+Math.cos((Math.PI*2)/verts*i)*(radius+tool))})
+   }
+   else if((i>25)&&(i<=50)){
+      frame1.push({X:((xmax+fo-radius)+Math.sin((Math.PI*2)/verts*i)*radius),Y:((ymin-fo+radius)+Math.cos((Math.PI*2)/verts*i)*radius)})
+      
+      path1.push({X:((xmax-radius)+Math.sin((Math.PI*2)/verts*i)*(radius+tool)),Y:((ymin+radius)+Math.cos((Math.PI*2)/verts*i)*(radius+tool))}) 
+   }
+   else if((i>50)&&(i<=75)){
+      frame1.push({X:((xmin-fo+radius)+Math.sin((Math.PI*2)/verts*i)*radius),Y:((ymin-fo+radius)+Math.cos((Math.PI*2)/verts*i)*radius)})
+      
+      path1.push({X:((xmin+radius)+Math.sin((Math.PI*2)/verts*i)*(radius+tool)),Y:((ymin+radius)+Math.cos((Math.PI*2)/verts*i)*(radius+tool))})     
+   }
+   else if((i>75)&&(i<=100)){
+      frame1.push({X:((xmin-fo+radius)+Math.sin((Math.PI*2)/verts*i)*radius),Y:((ymax+fo-radius)+Math.cos((Math.PI*2)/verts*i)*radius)})
+      
+      path1.push({X:((xmin+radius)+Math.sin((Math.PI*2)/verts*i)*(radius+tool)),Y:((ymax-radius)+Math.cos((Math.PI*2)/verts*i)*(radius+tool))})  
+           
+      if(i==100){
+      	frame1.push(frame1[0])
+      	path1.push(path1[0])
+      }      
+   }   
+}
+
+if(radius==0){
+	frame1=[]
+	frame1.push({X:xmax,Y:ymax})
+	frame1.push({X:xmax,Y:ymin})
+	frame1.push({X:xmin,Y:ymin})
+	frame1.push({X:xmin,Y:ymax})
+	frame1.push({X:xmax,Y:ymax})
+}
+
+else if(radius+tool<=0){
+	path1=[]
+	path1.push({X:-xmax-tool,Y:-ymax-tool})
+	path1.push({X:-xmax-tool,Y:-ymin+tool})
+	path1.push({X:-xmin+tool,Y:-ymin+tool})
+	path1.push({X:-xmin+tool,Y:-ymax-tool})
+	path1.push({X:-xmax-tool,Y:-ymax-tool})
+}
+	frame1.reverse()
+	draw()
+}
+
+////
+
+function makeGrid(){
+	pts = []
+	grid = []
+
+if((document.getElementById("board").value)=="arduino"){
+	pinNum = 0
+
+	y=2.54+(parseInt(document.getElementById("y-bot").value)*2.54)
+
+	for(i=0;i<20+(parseInt(document.getElementById("y-top").value))+(parseInt(document.getElementById("y-bot").value));i++){
+
+		x=0-(parseInt(document.getElementById("x-left").value)*2.54)
+	for(j=0;j<20+(parseInt(document.getElementById("x-right").value))+(parseInt(document.getElementById("x-left").value));j++){
+
+		//
+		if( ((i==(parseInt(document.getElementById("y-bot").value))) || (i==(parseInt(document.getElementById("y-bot").value)+1))) && (j>4+(parseInt(document.getElementById("x-left").value))) & (j<20+(parseInt(document.getElementById("x-left").value)))){
+			if(j==13){
+				k=pinNum
+				pinNum=0
+			}
+			else if(j==14){
+				pinNum=k+1
+			}
+			else{
+				pinNum++
+			}
+		}
+		else if( ((i==(parseInt(document.getElementById("y-bot").value)+18)) || (i==(parseInt(document.getElementById("y-bot").value)+19))) && (j>0+(parseInt(document.getElementById("x-left").value))) && (j<20+(parseInt(document.getElementById("x-left").value)))){
+
+			if((j==11)&&(i==(parseInt(document.getElementById("y-bot").value)+18))){
+				pinNum=0
+			}
+			else{
+				k++
+				pinNum=k
+			}
+		}
+		else{
+			k=14
+			pinNum=0
+		}
+		if(j>0){
+		pts.push({X:-19.685+o+x,Y:21.61+y,PIN:pinNum})
+		}
+		//
+		
+		if( (i==19+(parseInt(document.getElementById("y-bot").value))) && (j==(parseInt(document.getElementById("x-left").value))) ){
+			x = x + 2.54+0.635
+		}
+		else if( (i==19+(parseInt(document.getElementById("y-bot").value))) && (j==(10+(parseInt(document.getElementById("x-left").value)))) ){
+			x = x + 2.54+1.27+0.635
+			j++
+		}
+		else{
+			x = x+2.54
+		}
+
+	}
+		y = y-2.54
+	}
+
+	y = 3.79
+	for(i=0;i<20+(parseInt(document.getElementById("y-top").value))+(parseInt(document.getElementById("y-bot").value));i++){
+		grid.push([{X:-19.685+2.54-(parseInt(document.getElementById("x-left").value)*2.54)+o,Y:25.4+2.54+(parseInt(document.getElementById("y-bot").value)*2.54)-y},{X:29.845-2.54+o+1.27+(parseFloat(document.getElementById("x-right").value)*2.54),Y:25.4+2.54+(parseInt(document.getElementById("y-bot").value)*2.54)-y}])
+		y = y + 2.54
+	}
+
+	x = 0
+	for(i=0;i<19+(parseInt(document.getElementById("x-right").value))+(parseInt(document.getElementById("x-left").value));i++){
+		grid.push([{X:-19.685+2.54-(parseInt(document.getElementById("x-left").value)*2.54)+o+x,Y:21.61+2.54+(parseInt(document.getElementById("y-bot").value)*2.54)},{X:-19.685+2.54-(parseInt(document.getElementById("x-left").value)*2.54)+o+x,Y:-21.61-2.54-(parseInt(document.getElementById("y-top").value)*2.54)}])
+		x = x + 2.54
+	}	
+
+	}//
+	else if(document.getElementById("board").value=="blank"){
+	
+		for(i=0;i<(Math.round(((Math.abs(ymin))+ymax)/2.54));i++){
+			for(j=0;j<(Math.round(((Math.abs(xmin))+xmax)/2.54));j++){
+				pts.push({X:(xmin+1.27+(j*2.54)),Y:(ymax-1.27-(i*2.54))})
+			}
+		}
+
+	//grid
+
+	for(i=0;i<(Math.round(((Math.abs(ymin))+ymax)/2.54));i++){
+		grid.push([{X:xmin+1.27,Y:(ymax-1.27-(i*2.54))},{X:xmax-1.27,Y:(ymax-1.27-(i*2.54))}])
+	}
+
+	for(i=0;i<(Math.round(((Math.abs(xmin))+xmax)/2.54));i++){
+		grid.push([{X:(xmin+1.27+(i*2.54)),Y:(ymax-1.27)},{X:(xmin+1.27+(i*2.54)),Y:(ymin+1.27)}])
+	}
+		
+
+	}
+
+}
+
+
+
+
